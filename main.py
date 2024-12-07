42ac488313225c3f6c89e686f91f8e73a69140ad
MZK6B3@NAM.CORP.GM.COM <MZK6B3@NAM.CORP.GM.COM>
2024-12-07 11:55:36 -0600

    changed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
 
-
+#sample application to demonstrate the use of Jinja2Templates
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
